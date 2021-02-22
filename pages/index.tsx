77d24e56d6b594617bc7c484b21d97e5d04a21cc
manticorevault <artur_seilah@hotmail.com>
2021-02-22 21:00:12 +0000

    Retrieving post titles from the Ghost CMS and displaying them in the front end.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,51 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const BLOG_URL = process.env.BLOG_URL
+const CONTENT_API_KEY = process.env.CONTENT_API_KEY
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+type Post = {
+  title: string
+  slug: string
 }
+
+async function fetchPosts() {
+  const res = await fetch(
+    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt,reading_time`
+  ).then((res) => res.json())
+
+  const posts = res.posts
+
+  return posts
+}
+
+export const getStaticProps = async ({ params }) => {
+  const posts = await fetchPosts()
+  return {
+    props: { posts }
+  }
+}
+
+const Home: React.FC<{ posts: Post[] }> = (props) => {
+	const { posts } = props
+
+	return (
+		<div className={styles.container}>
+			<h1>Hello to my blog</h1>
+			<ul>
+				{posts.map((post, index) => {
+					return (
+						<li key={post.slug}>
+							<Link href="/post/[slug]" as={`/post/${post.slug}`}>
+								<a>{post.title}</a>
+							</Link>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	)
+}
+
+export default Home
